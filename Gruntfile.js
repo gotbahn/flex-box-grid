5749aaf9fee4e1d179a9cf10f5566346c18067de
Bogdan Plieshka <b.pleshka@gmail.com>
2015-06-05 13:24:05 +0300

    Initial

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    var version = 'patch';
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> by <%= pkg.author %>, <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.css': ['dist/<%= pkg.name %>.css']
+                }
+            }
+        },
+
+        version: {
+            options: {
+                release: version
+            },
+            src: [
+                'package.json',
+                'bower.json'
+            ]
+        },
+
+        exec: {
+            gitTag: {
+                cmd: 'git tag -a v<%= pkg.version %> -m "Package update"'
+            }
+        },
+
+        copy: {
+            scss: {
+                expand: true,
+                cwd: 'src/styles/',
+                src: '**',
+                dest: 'dist/scss/'
+            }
+        },
+
+        sass: {
+            flexgrid: {
+                files: {
+                    'dist/css/flexgrid.css': 'src/styles/flexgrid.scss'
+                }
+            },
+            demo: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'demo/styles/styles.css': 'demo/styles/styles.scss'
+                }
+            }
+        },
+
+        watch: {
+            files: ['**/*.scss'],
+            tasks: ['sass']
+        }
+
+    });
+
+    grunt.registerTask('build', function () {
+        grunt.task.run([
+            'uglify',
+            'copy',
+            'version'
+        ]);
+        if (version !== 'patch') {
+            grunt.task.run('exec:gitTag');
+        }
+    });
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# flexgrid
+# FlexGrid
+
 Grid system with flexible boxes
+
+## Contributing
+
+I'll check out your contribution if you:
+
+* Provide a comprehensive suite of tests for your fork.
+* Have a clear and documented rationale for your changes.
+* Package these up in a pull request.
+
+## License
+
+MIT
