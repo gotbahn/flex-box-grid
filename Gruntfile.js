615a8014e4e038404d1a26cb180913c2d77b157a
Bogdan Plieshka <b.pleshka@gmail.com>
2015-06-06 15:10:43 +0300

    flexgrid core styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,6 +75,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build', function () {
         grunt.task.run([
+            'sass',
             'uglify',
             'copy',
             'version'
